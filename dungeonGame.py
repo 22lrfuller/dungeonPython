da345ab440dc7dbd89c33188a024672cb916cc05
22lrfuller <lfuller10@outlook.com>
2018-04-11 10:57:22 -0400

    Update dungeonGame.py

--- dungeonGame.py
+++ dungeonGame.py
@@ -51,18 +51,6 @@ yellow = (255, 255, 0)
 green = (0, 210, 0)
 blue = (0, 125, 255)
 
-def printBoard():
-	column_width = 16
-	print("     ", end="")
-	for x in range(xSize):
-		print("x={}".format(x).ljust(column_width), end="")
-	sys.stdout.write("\n")
-	for y in range(ySize):
-		print("y={} (".format(y), end="")
-		for x in range(xSize):
-			    print("{}".format(gameBoard[y][x]).ljust(column_width), end="")
-		print(")")
-
 def getBlockType(blockX, blockY, blockKind): # Defining blocks in the grid
 	global entrancePos
 	if(blockKind == 'origPath'):
