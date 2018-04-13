61be2e4b92ff8b347cb9e669dc2abadd7799f42f
22lrfuller <lfuller10@outlook.com>
2018-04-12 20:02:09 -0400

    Cleaned code, finished inventory, started on adding an animation.

--- dungeonGame.py
+++ dungeonGame.py
@@ -29,19 +29,21 @@ entrancePos = []
 posPath = []
 
 # Images
-startScreen = pygame.transform.scale(pygame.image.load('gameArt/startScreen.png'),(1000,1000))
+startScreen = pygame.transform.scale(pygame.image.load('gameArt/startScreen.png'), (1000,1000))
 playerImg = pygame.transform.scale(pygame.image.load('gameArt/pixelChar.png'), (75, 75))
+invBG = pygame.transform.scale(pygame.image.load('gameArt/invBG.png'), (1200,100))
+invObj = pygame.transform.scale(pygame.image.load('gameArt/invObj.png'), (75,75))
 
 path = pygame.transform.scale(pygame.image.load('gameArt/path.png'), (100,100))
 crackedPath = pygame.transform.scale(pygame.image.load('gameArt/cracked.path.png'), (100,100))
 flowerPath = pygame.transform.scale(pygame.image.load('gameArt/flower.path.png'), (100,100))
-crackedFlowerPath = pygame.transform.scale(pygame.image.load('gameArt/crackedFlower.path.png'),(100,100))
-aBlock = pygame.transform.scale(pygame.image.load('gameArt/aBlock.png'),(100,100))
-flowerABlock = pygame.transform.scale(pygame.image.load('gameArt/flower.aBlock.png'),(100,100))
-wall = pygame.transform.scale(pygame.image.load('gameArt/wall.png'),(100,100))
-crackedWall = pygame.transform.scale(pygame.image.load('gameArt/cracked.wall.png'),(100,100))
-door = pygame.transform.scale(pygame.image.load('gameArt/door.png'),(100,100))
-flowerDoor = pygame.transform.scale(pygame.image.load('gameArt/flower.door.png'),(100,100))
+crackedFlowerPath = pygame.transform.scale(pygame.image.load('gameArt/crackedFlower.path.png'), (100,100))
+cBlock = pygame.transform.scale(pygame.image.load('gameArt/cBlock.png'), (100,100))
+flowerCBlock = pygame.transform.scale(pygame.image.load('gameArt/flower.cBlock.png'), (100,100))
+wall = pygame.transform.scale(pygame.image.load('gameArt/wall.png'), (100,100))
+crackedWall = pygame.transform.scale(pygame.image.load('gameArt/cracked.wall.png'), (100,100))
+door = pygame.transform.scale(pygame.image.load('gameArt/door.png'), (100,100))
+flowerDoor = pygame.transform.scale(pygame.image.load('gameArt/flower.door.png'), (100,100))
 
 # Colors
 white = (255, 255, 255)
@@ -51,34 +53,22 @@ yellow = (255, 255, 0)
 green = (0, 210, 0)
 blue = (0, 125, 255)
 
-def printBoard():
-	column_width = 16
-	print("     ", end="")
-	for x in range(xSize):
-		print("x={}".format(x).ljust(column_width), end="")
-	sys.stdout.write("\n")
-	for y in range(ySize):
-		print("y={} (".format(y), end="")
-		for x in range(xSize):
-			    print("{}".format(gameBoard[y][x]).ljust(column_width), end="")
-		print(")")
-
 def getBlockType(blockX, blockY, blockKind): # Defining blocks in the grid
 	global entrancePos
 	if(blockKind == 'origPath'):
-		randBlock = randNum(1,5)
-		if(randBlock <= 4): return 'path'
-		return 'actionBlock'
+		randBlock = randNum(1,20) # 95% chance for path, 5% for chestBlock
+		if(randBlock <= 19): return 'path'
+		return 'chestBlock'
 	if(blockKind == 'other'):
 		if(blockX == 0 or blockX == xMax or blockY == 0 or blockY == yMax):
 			if(blockY == yMax and randDoor == blockX):
 				entrancePos = [randDoor,blockY]
 				return 'entrance'
 			return 'border'
-		randBlock = randNum(1,25)
-		if(randBlock <= 9): return 'path'
-		if(randBlock > 9 and randBlock < 24): return 'wall'
-		return 'actionBlock'
+		randBlock = randNum(1,40)
+		if(randBlock <= 20): return 'path'
+		if(randBlock > 20 and randBlock < 39): return 'wall'
+		return 'chestBlock'
 
 def getArt(blockKind): # Gets the Random Art for that type
 	if(blockKind == 'path'):
@@ -87,10 +77,10 @@ def getArt(blockKind): # Gets the Random Art for that type
 		if(randomVar == 2): return 'cracked.path'
 		if(randomVar == 3): return 'flower.path'
 		if(randomVar == 4): return 'crackedFlower.path'
-	if(blockKind == 'actionBlock'):
+	if(blockKind == 'chestBlock'):
 		randomVar = randNum(1,2)
-		if(randomVar == 1): return 'aBlock'
-		if(randomVar == 2): return 'flower.aBlock'
+		if(randomVar == 1): return 'cBlock'
+		if(randomVar == 2): return 'flower.cBlock'
 	if(blockKind == 'wall' or blockKind == 'border'):
 		randomVar = randNum(1,2)
 		if(randomVar == 1): return 'wall'
@@ -107,15 +97,13 @@ def getDir(entrancePos): # Creating the random path
 	print(int(round(xSize/2.0)))
 	while(blockY-1 != 0):
 		randomVar = randNum(1,3)
-		if blockX+1 > xMax:
-			import pdb; pdb.set_trace()
 		if(randomVar == 1 and gameBoard[blockY][blockX-1][0] != 'border'): # Left
 			blockX -= 1
 		if(randomVar == 2 and gameBoard[blockY-1][blockX][0] != 'border'): # Up
 			blockY -= 1
 		if(randomVar == 3 and gameBoard[blockY][blockX+1][0] != 'border'): # Right
 			blockX += 1
-		while(blockY-1 == 0 and blockX < int(round((xSize-1)/2.0))):
+		while(blockY-1 == 0 and blockX < int(round(xSize-(xSize/4.0)))):
 			blockX += 1
 			posPath.append([blockX, blockY])
 		posPath.append([blockX, blockY])
@@ -143,26 +131,27 @@ def keyUp(event, funcX, funcY): # Start Movement on Key Down
 				funcX += 1
 	return funcX, funcY
 
+def checkArea(xBox,yBox):
+	if(gameBoard[yBox][xBox][0] == 'cBlock'):
+		pass
+
 # Creating the game board in a row-major format
 # Everything will be referenced [y][x] in the array
 gameBoard = [[[''] for x in range(xSize)] for y in range(ySize)]
 for loopY in range(ySize):
 	randDoor = randNum(1,int(round((xSize-1)/4.0)))
 	for loopX in range(xSize):
-		blockType = getBlockType(loopX,loopY,'other')
-		gameBoard[loopY][loopX][0] = blockType
+		gameBoard[loopY][loopX][0] = getBlockType(loopX,loopY,'other')
 
 getDir(entrancePos) #Generates Random Path
-# printBoard()
 
 # Making art for the game after the path is generated
 for loopY in range(0,ySize):
 	for loopX in range(0,xSize):
 		if(loopX == entrancePos[0] and loopY == entrancePos[1]):
-			art = getArt('entrance')
+			gameBoard[loopY][loopX].append(getArt('entrance'))
 		else:
-			art = getArt(gameBoard[loopY][loopX][0])
-		gameBoard[loopY][loopX].append(art)
+			gameBoard[loopY][loopX].append(getArt(gameBoard[loopY][loopX][0]))
 
 xBox, yBox = entrancePos # Sets the player's position
 
@@ -181,7 +170,10 @@ while(gameOver == False):
 			if(startButton.collidepoint(mousePos)):
 				scene = 'game'
 		if(event.type == pygame.KEYUP and scene == 'game'):
-			xBox, yBox = keyUp(event, xBox, yBox)
+			if(event.key == K_UP or event.key == K_w or event.key == K_DOWN or event.key == K_s or event.key == K_LEFT or event.key == K_a or event.key == K_RIGHT or event.key == K_d):
+				xBox, yBox = keyUp(event, xBox, yBox)
+			elif(event.key == K_SPACE):
+				checkArea(xBox,yBox)
 		if(event.type == pygame.QUIT):
 			gameOver = True
 	screen.fill(white)
@@ -193,12 +185,11 @@ while(gameOver == False):
 		for y in range(len(gameBoard)):
 			for x in range(len(gameBoard[y])):
 				screen.blit(pygame.transform.scale(pygame.image.load('gameArt/' + str(gameBoard[y][x][1]) + '.png'),(100,100)),(x*100, y*100))
-		screen.blit(pygame.transform.scale(pygame.image.load('gameArt/invBG.png'),(1200,100)),(0,800))
-		# for i in range(xSize-1): #TODO add to the inventory
-			# if(i != 0 or i != xSize-1):
-
+		screen.blit(invBG,(0,800))
+		for x in range(10): # Creating boxes in the inventory
+			if(x != 0 or x != xMax-1):
+				screen.blit(invObj,((x+1)*110,812))
 		screen.blit(playerImg,(xBox*100+12, yBox*100+12))
-
 	pygame.display.update()
 	clock.tick(60)
 endGame()
